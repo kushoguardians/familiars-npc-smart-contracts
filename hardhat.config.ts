04b685c66e52f74817300947abe7a1b33eebd385
exidz <exidz@protonmail.com>
2024-11-09 22:41:43 +0800

    chore: config update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,17 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+
+const PRIVATEKEY_DEV = vars.get("PRIVATEKEY_DEV");
+const BASE_SEPOLIA_RPC = vars.get("BASE_SEPOLIA_RPC");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
-  
+  networks: {
+    baseSepolia: {
+      url: BASE_SEPOLIA_RPC,
+      accounts: [PRIVATEKEY_DEV],
+    },
+  },
 };
 
 export default config;
