0a381befcc83d2302c0215565b113d3c32f83773
exidz <exidz@protonmail.com>
2024-11-10 22:19:59 +0800

    chore: verify contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,10 @@
 import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-verify";
 
 const PRIVATEKEY_DEV = vars.get("PRIVATEKEY_DEV");
 const BASE_SEPOLIA_RPC = vars.get("BASE_SEPOLIA_RPC");
+const BLOCKSCOUT_API_KEY = vars.get("BLOCKSCOUT_API_KEY");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
@@ -11,6 +13,21 @@ const config: HardhatUserConfig = {
       url: BASE_SEPOLIA_RPC,
       accounts: [PRIVATEKEY_DEV],
     },
+  },
+  etherscan: {
+    apiKey: {
+      "base-sepolia": BLOCKSCOUT_API_KEY,
+    },
+    customChains: [
+      {
+        network: "base-sepolia",
+        chainId: 84532,
+        urls: {
+          apiURL: "https://base-sepolia.blockscout.com/api",
+          browserURL: "https://base-sepolia.blockscout.com",
+        },
+      },
+    ],
   },
 };
 
